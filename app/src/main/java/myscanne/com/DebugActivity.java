0b65c4140f96fa05a27224ee38fddb41ba91a3e4
AmbitiousBoyII14 <132277700+AmbitiousBoyII14@users.noreply.github.com>
2026-08-29 10:10:01 +0200

    upload app/src/main/java/myscanne/com/DebugActivity.java

---
+++ app/src/main/java/myscanne/com/DebugActivity.java
@@ -0,0 +1,83 @@
+package myscanne.com;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class DebugActivity extends Activity {
+
+    String[] exceptionType = {
+            "StringIndexOutOfBoundsException",
+            "IndexOutOfBoundsException",
+            "ArithmeticException",
+            "NumberFormatException",
+            "ActivityNotFoundException"
+    };
+
+    String[] errMessage = {
+            "Invalid string operation\n",
+            "Invalid list operation\n",
+            "Invalid arithmetical operation\n",
+            "Invalid toNumber block operation\n",
+            "Invalid intent operation"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        String errMsg = "";
+        String madeErrMsg = "";
+
+        if (intent != null) {
+            errMsg = intent.getStringExtra("error");
+
+            if (errMsg == null) {
+                errMsg = "";
+            }
+
+            String[] split = errMsg.split("\n");
+
+            try {
+                for (int j = 0; j < exceptionType.length; j++) {
+                    if (split[0].contains(exceptionType[j])) {
+                        madeErrMsg = errMessage[j];
+
+                        int addIndex = split[0].indexOf(exceptionType[j])
+                                + exceptionType[j].length();
+
+                        if (addIndex < split[0].length()) {
+                            madeErrMsg += split[0].substring(addIndex);
+                        }
+
+                        break;
+                    }
+                }
+
+                if (madeErrMsg.length() == 0) {
+                    madeErrMsg = errMsg;
+                }
+            } catch (Exception e) {
+                madeErrMsg = errMsg;
+            }
+        }
+
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+
+        bld.setTitle("An error occurred");
+        bld.setMessage(madeErrMsg);
+
+        bld.setNeutralButton("End Application", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        bld.create().show();
+    }
+}
